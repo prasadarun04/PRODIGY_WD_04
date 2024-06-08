c10191e004707fec5051e1bb11d1e9b88a0e6165
prasadarun04 <162180756+prasadarun04@users.noreply.github.com>
2024-06-08 10:25:00 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Modal
